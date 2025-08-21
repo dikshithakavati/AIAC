6fd38e9d6c8d9d9ea73284a6e702ec73d63c73d1
2403A51031 <2403a51031@sru.edu.in>
2025-08-21 10:19:44 +0530

    Delete lab6.3 directory

--- lab6.3/task2.py
+++
@@ -1,9 +0,0 @@
-def multiple(num, start, end):
-    for i in range(start, end+1):
-        if i<=end and i>=start :
-            print(num*i)
-            i=i+1
-a=int(input("Enter the number: "))
-start=int(input("Enter the start number: "))
-end=int(input("Enter the end number: "))
-multiple(a, start, end)
--- lab6.3/task4.py
+++
@@ -1,8 +0,0 @@
-def  sum_to_n(n):
-    sum=0
-    while n>0:
-        sum=sum+n
-        n=n-1
-    print(sum)
-a=int(input("Enter the number: "))
-sum_to_n(a)
